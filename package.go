e554d88effda0ca1747d6ad52b3d33144600b31a
Ryan Leckey <ryan@launchbadge.com>
2018-11-21 11:50:20 -0800

    Point to musl instead of gnu lib for linux

--- package.go
+++ package.go
@@ -3,7 +3,7 @@ package hedera
 // #cgo CFLAGS: -I ./include
 // #cgo LDFLAGS: -l hedera
 // #cgo darwin LDFLAGS: -L libs/x86_64-apple-darwin -framework Security -l c++
-// #cgo linux LDFLAGS: -L libs/x86_64-unknown-linux-gnu -l stdc++ -l m -l dl -l resolv
+// #cgo linux LDFLAGS: -L libs/x86_64-unknown-linux-musl
 // #cgo windows LDFLAGS: -L libs/x86_64-pc-windows-gnu -l ws2_32 -l iphlpapi -l dbghelp -l userenv
 // #include <stdlib.h>
 // #include "hedera-error.h"
