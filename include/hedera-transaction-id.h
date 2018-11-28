bf72ea0937f1befc261f3233c4c1e3c99a0aae88
Ryan Leckey <ryan@launchbadge.com>
2018-11-27 16:07:37 -0800

    sync libs/ and include/ from hedera-sdk-c#e557e66

--- include/hedera-transaction-id.h
+++ include/hedera-transaction-id.h
@@ -17,6 +17,12 @@ extern HederaTransactionId hedera_transaction_id_new(HederaAccountId account);
 
 extern char* hedera_transaction_id_to_str(HederaTransactionId*);
 
+/// Parse a [HederaTransactionID] from a string.
+///
+/// Returns [HEDERA_ERROR_SUCCESS] (0) on success or any other value on error. Use [hedera_error_message] to retrieve
+/// a message for the error.
+extern HederaError hedera_transaction_id_from_str(const char* s, HederaTransactionId* out);
+
 #ifdef __cplusplus
 }
 #endif
