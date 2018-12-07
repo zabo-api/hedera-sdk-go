1555f4e882873961542db4e2bcd02c1f0adaea51
Ryan Leckey <ryan@launchbadge.com>
2018-12-07 00:30:58 -0800

    Add new build system

--- README.md
+++ README.md
@@ -6,25 +6,25 @@
  * Go 1.11+
 
  * [Git LFS](https://git-lfs.github.com) – Git LFS is used to version the built libraries in [`./libs`](./libs) (built from `hedera-sdk-rust`). This must be installed on your system prior to `go get`.
- 
+
     - MacOS (homebrew)
-    
+
         ```sh
         $ brew install git-lfs
         $ git lfs install
         ```
- 
+
     - Ubuntu / Debian
-        
+
         ```sh
         $ sudo apt install git-lfs
         $ git lfs install
         ```
-        
+
     - Windows
-        
+
         Download and run [git-lfs.exe](https://github.com/git-lfs/git-lfs/releases/download/v2.6.0/git-lfs-windows-v2.6.0.exe). Open your git console ( under `Git Bash` if you installed Git through https://git-scm.com/ ).
-        
+
         ```sh
         $ git lfs install
         ```
@@ -35,9 +35,37 @@
 $ go get github.com/hashgraph/hedera-sdk-go
 ```
 
+## Development
+
+#### Requirements
+
+ * [rustup](https://rustup.rs/)
+
+ * `musl`
+
+    - MacOS
+
+        ```sh
+        $ brew install FiloSottile/musl-cross/musl-cross
+        ```
+
+ * `MinGW`
+
+    - MacOS
+
+        ```sh
+        $ brew install mingw-w64
+        ```
+
+#### Build
+
+```sh
+$ ./x.py
+```
+
 ## License
 
-Licensed under Apache License, 
+Licensed under Apache License,
 Version 2.0 ([LICENSE](LICENSE) or http://www.apache.org/licenses/LICENSE-2.0).
 
 ## Contribution
---
+++ x.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python3
+import os, re, argparse, sys
+from subprocess import Popen, check_call, PIPE, check_output, CalledProcessError
+from shutil import copy2, copytree, rmtree
+
+
+def sh(command, silent=False, cwd=None, shell=True, env=None):
+    if env is not None:
+        env = dict(**os.environ, **env)
+
+    if silent:
+        p = Popen(
+            command, shell=shell, stdout=PIPE, stderr=PIPE, cwd=cwd, env=env)
+        stdout, _ = p.communicate()
+
+        return stdout
+
+    else:
+        check_call(command, shell=shell, cwd=cwd, env=env)
+
+
+def update_submodules():
+    print(" :: update hedera-sdk-c")
+
+    # Ensure the submodule is initialized
+    sh("git submodule update --init", silent=True)
+
+    # Fetch upstream changes
+    sh("git submodule foreach git fetch", silent=True)
+
+    # Reset to upstream
+    sh("git submodule foreach git reset origin/HEAD", silent=True)
+
+    # Update include/
+    rmtree("include")
+    copytree("vendor/hedera-sdk-c/include", "include")
+
+
+def get_default_target():
+    targets = sh("rustup target list", silent=True).decode()
+    m = re.search(r"(.*?)\s*\(default\)", targets)
+
+    return m[1]
+
+
+def build(release=False):
+    default_target = get_default_target()
+    targets = [
+        "x86_64-apple-darwin", "x86_64-pc-windows-gnu",
+        "x86_64-unknown-linux-musl"
+    ]
+
+    prefix = {
+        "x86_64-apple-darwin": "",
+        "x86_64-pc-windows-gnu": "x86_64-w64-mingw32-",
+        "x86_64-unknown-linux-musl": "x86_64-linux-musl-"
+    }
+
+    artifact = {
+        "x86_64-apple-darwin": "libhedera.a",
+        "x86_64-pc-windows-gnu": "hedera.lib",
+        "x86_64-unknown-linux-musl": "libhedera.a"
+    }
+
+    if release:
+        for target in targets:
+            print(f" :: build hedera-sdk-c for {target}")
+
+            if target != default_target:
+                sh(["rustup", "target", "add", target],
+                   shell=False,
+                   silent=True,
+                   cwd="vendor/hedera-sdk-c")
+
+            profile = "--release" if release else ''
+            sh(f"cargo build --target {target} {profile}",
+               cwd="vendor/hedera-sdk-c",
+               env={
+                   "CC": f"{prefix[target]}gcc",
+                   "CARGO_TARGET_X86_64_UNKNOWN_LINUX_MUSL_LINKER": f"{prefix[target]}gcc",
+                   "CARGO_TARGET_X86_64_PC_WINDOWS_GNU_LINKER": f"{prefix[target]}gcc",
+               })
+
+            if target.endswith("-apple-darwin"):
+                sh(f"strip -Sx {artifact[target]}",
+                   cwd=f"vendor/hedera-sdk-c/target/{target}/release", silent=True)
+
+            else:
+                sh(f"{prefix[target]}strip --strip-unneeded -d -x {artifact[target]}",
+                   cwd=f"vendor/hedera-sdk-c/target/{target}/release")
+
+            copy2(f"vendor/hedera-sdk-c/target/{target}/release/{artifact[target]}", f"libs/{target}/")
+
+    else:
+        # For development; build only the _default_ target
+        print(f" :: build hedera-sdk-c for {default_target}")
+        sh(f"cargo build --target {default_target}", cwd="vendor/hedera-sdk-c")
+
+        # Copy _default_ lib over
+        copy2(f"vendor/hedera-sdk-c/target/{target}/debug/{artifact[target]}", f"libs/{target}/")
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "-R", "--release", help="build in release mode", action="store_true")
+
+argv = parser.parse_args(sys.argv[1:])
+
+update_submodules()
+build(release=argv.release)
