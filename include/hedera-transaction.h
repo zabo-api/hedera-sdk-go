acf9fc15d038cc209157388b233de4e468b64659
Ryan Leckey <ryan@launchbadge.com>
2019-07-17 22:07:17 -0700

    build libs/ and sync include/ from hedera-sdk-c#3d21b0e

--- include/hedera-transaction.h
+++ include/hedera-transaction.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <stdint.h>
+
 #include "hedera-error.h"
 #include "hedera-crypto.h"
 #include "hedera-client.h"
@@ -17,6 +19,8 @@ extern void hedera_transaction_set_operator(HederaTransaction*, HederaAccountId 
 extern void hedera_transaction_set_node(HederaTransaction*, HederaAccountId node);
 
 extern void hedera_transaction_set_memo(HederaTransaction*, const char* memo);
+
+extern void hedera_transaction_set_fee(HederaTransaction*, uint64_t fee);
 
 extern void hedera_transaction_sign(HederaTransaction*, HederaSecretKey*);
 
